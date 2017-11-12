be838d5ef339e4795c4e722e5c30683265b7bd73
rlatjdfo112 <rlatjdfo112@naver.com>
2017-11-13 00:24:25 +0900

    Implement the post item

--- src/components/indexPage/IndexPage.js
+++ src/components/indexPage/IndexPage.js
@@ -5,20 +5,52 @@ import styles from './Index.css';
 import axios from 'axios';
 
 import Header from './header/Header';
+import PostItem from './postItem/PostITem';
 
+import InfiniteScroll from 'react-infinite-scroller';
+
+
+function createItem(page) {
+  return (<PostItem id={page + 1} />)
+}
 
 class IndexPage extends React.Component{
 
   constructor(props){
     super(props);
 
-    this.state={}
+    this.state={
+      hasMorePost: true,
+      items: [createItem(0)]
+    }
+    this.loadPostItem = this.loadPostItem.bind(this);
+  }
+
+  loadPostItem(page){
+    setTimeout(function() {
+      this.setState({
+        items: this.state.items.concat([createItem(page)]),
+        hasMore: (page < 1000)
+      });
+    }.bind(this), 100);
   }
 
   render(){
+    let renderInfiniteContents = () => {
+
+    }
     return (
       <div>
-      <Header/>
+        <Header/>
+        <div className={styles.contents}>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadPostItem}
+            hasMore={this.state.hasMorePost}
+            loader={<div className="loader">Loading ...</div>}>
+            {this.state.items}
+          </InfiniteScroll>
+        </div>
       </div>
     )
   }
--- src/components/indexPage/header/Header.js
+++ src/components/indexPage/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Header.css';
 
 const propTypes = {
-
+   
 }
 
 class Header extends React.Component{
