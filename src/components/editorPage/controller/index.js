b0631a3ab166d0748c73f55a3ac90034157ad96c
rlatjdfo112 <rlatjdfo112@naver.com>
2017-11-16 23:57:55 +0900

    [Impl] Editor controller modal

--- src/components/editorPage/controller/index.js
+++ src/components/editorPage/controller/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import axios from 'axios';
 import styles from './controller.css';
+import LocationController from './LocationController';
+
+const Weather = 'weather-modal';
+const Image = 'image-modal';
+const Location = 'location-modal';
+const Line = 'line-modal';
 
 class Controller extends React.Component{
 
@@ -8,16 +14,44 @@ class Controller extends React.Component{
     super(props);
 
     this.state = {
+      modal: ''
     }
   }
 
+  toggleDialog(e, modal){
+    let targetElement = e.target.getBoundingClientRect();
+    let parentElement = e.target.parentNode.getBoundingClientRect();
+    let x = targetElement.left - parentElement.left;
+    let y = targetElement.top - parentElement.top;
+    this.setState({
+      modal: (this.state.modal == modal)? undefined: modal,
+      x: x+45,
+      y: y-50
+    });
+  }
+
   render(){
+    let getModal = (modal) => {
+      switch (modal){
+        case Weather:
+          return <LocationController/>;
+      }
+        return <LocationController/>;
+    }
+    let renderModal = (modal) => {
+      if(modal){
+      return (<div key={modal} style={{'right':this.state.x, 'top':this.state.y}} className={styles.controller_modal}><div className={styles.context}>
+          {getModal()}
+          </div></div>)
+      }
+    }
     return (
       <div className={styles.content_controller}>
-        <img className={styles.item} src={'/resources/writing_view/weather_btn.svg'}/>
-        <img className={styles.item} src={'/resources/writing_view/img_btn.svg'}/>
-        <img className={styles.item} src={'/resources/writing_view/map_btn.svg'}/>
-        <img className={styles.item} src={'/resources/writing_view/line_btn.svg'}/>
+        <img className={styles.item} onClick={(e)=>this.toggleDialog(e, Weather)} src={'/resources/writing_view/weather_btn.svg'}/>
+        <img className={styles.item} onClick={(e)=>this.toggleDialog(e, Image)} src={'/resources/writing_view/img_btn.svg'}/>
+        <img className={styles.item} onClick={(e)=>this.toggleDialog(e, Location)} src={'/resources/writing_view/map_btn.svg'}/>
+        <img className={styles.item} onClick={(e)=>this.toggleDialog(e, Line)} src={'/resources/writing_view/line_btn.svg'}/>
+        {renderModal(this.state.modal)}
       </div>
     )
   }
---
+++ src/components/editorPage/controller/locationController/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+import styles from './LocationController.css';
+
+class LocationController extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render(){
+    return (
+      <div className={styles.locationController}>
+        asdasd
+      </div>
+    )
+  }
+
+}
+export default LocationController;
